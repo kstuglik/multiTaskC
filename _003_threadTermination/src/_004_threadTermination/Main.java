9a99c6060888e6da4869deca856671c4df489380
zorro <zorro@lenovo>
2019-10-10 22:57:27 +0200

    commit_004: THREAD TERMINATION

---
+++ _003_threadTermination/src/_004_threadTermination/Main.java
@@ -0,0 +1,24 @@
+package _004_threadTermination;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread thread = new Thread(new BlockingTask());
+		thread.start();
+		thread.interrupt();
+	}
+	
+	private static class BlockingTask implements Runnable{
+		@Override
+		public void run() {
+			try {
+				Thread.sleep(5000);
+				System.out.println("koniec przetwarzania bloku zadan");
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				System.out.println("Exit from BlockingTask Thread");
+			}
+		}
+	}
+}
